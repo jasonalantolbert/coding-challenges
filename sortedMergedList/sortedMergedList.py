c59e788bcc065143e4b22657e0a45083a1d8080b
Jason Tolbert Jr <jasonalantolbert@gmail.com>
2020-11-18 18:06:52 -0500

    Update sortedMergedList.py

--- sortedMergedList/sortedMergedList.py
+++ sortedMergedList/sortedMergedList.py
@@ -3,7 +3,3 @@ def sort_and_merge(array):
     for i in array:
         new_list.extend(i)
     return sorted(new_list)
-
-
-lists = [[1], [1, 3, 5], [1, 10, 20, 30, 40]]
-print(sort_and_merge(lists))
