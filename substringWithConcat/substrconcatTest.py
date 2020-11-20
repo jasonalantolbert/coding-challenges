c5c5ea1fc40214cde9997cffe778e113b41c395c
Jason Tolbert <jasonalantolbert@gmail.com>
2020-11-19 22:51:50 -0500

    updated substrconcatTest.py

--- substringWithConcat/substrconcatTest.py
+++ substringWithConcat/substrconcatTest.py
@@ -4,11 +4,28 @@ from substringWithConcat.substrconcat import find_substring
 
 
 class MyTestCase(unittest.TestCase):
-    def test_substrconcat(self):
+    def test1(self):
+        s = "barfoothefoobarman"
+        words = ["foo", "bar"]
+
+        expected = {0, 9}
+        actual = set(find_substring(s, words))
+        self.assertEqual(expected, actual)
+
+    def test2(self):
+        s = "wordgoodgoodgoodbestword"
+        words = ["word", "good", "best", "word"]
+
+        expected = set()
+        actual = set(find_substring(s, words))
+        self.assertEqual(expected, actual)
+
+    def test3(self):
         s = "barfoofoobarthefoobarman"
         words = ["bar", "foo", "the"]
-        actual = find_substring(s, words)
-        expected = [6, 9, 12]
+
+        expected = {6, 9, 12}
+        actual = set(find_substring(s, words))
         self.assertEqual(expected, actual)
 
 
